ba0f7ab95a5cb839404e9abb1af6f6d935578ee8
Vladimir Stanotin <swan9250@gmail.com>
2021-09-23 09:32:28 +0000

    fix work message

--- work.py
+++ work.py
@@ -61,18 +61,16 @@ def neus(context):
             people_on_line = "ALARM!!! Никого нет на линии!"
 #        self.bot.sendMessage(Main.neustroev_chat_id, pizdit + '\n\n' + tickets_count + '\n\n' + people_on_line)
 #        self.bot.sendMessage(Main.swans_chat_id, pizdit + '\n\n' + tickets_count + '\n\n' + people_on_line)
-        context.user_data['response'] = pizdit + '\n\n' + tickets_count + '\n\n' + people_on_line
-        return context.user_data['response']
+        return pizdit + '\n\n' + tickets_count + '\n\n' + people_on_line
 
 
 def run(context):
     queue = context.user_data['queue']
-#    try:
-    enter = auth.Auth()
-    response = queue.run_repeating(neus, 60, last=datetime.time(hour = 17, minute = 0, second = 00), context=context, name='Work').context.user_data['response']
-    print(dir(response))
-    queue.get_jobs_by_name('Work')[0].enabled = True
-    print(enter, response)
-    return "Уведомления включены\n" + response
-#    except:
-#        return "Что-то пошло не так, обратитесь к разработчику"
+    try:
+        auth.Auth()
+        response = queue.run_repeating(neus, 60, last=datetime.time(hour = 17, minute = 0, second = 00), context=context, name='Work')
+        queue.get_jobs_by_name('Work')[0].enabled = True
+        print(enter, response)
+        return "Уведомления включены\n", response
+    except:
+        return "Что-то пошло не так, обратитесь к разработчику"
